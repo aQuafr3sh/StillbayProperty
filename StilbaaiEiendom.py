6c8a3831ef515ff816c8a95319c64c66e4ad39df
aQuafr3sh <aquafr3sh@gmail.com>
2021-12-06 21:08:31 +0200

    Created initial docstring

---
+++ StilbaaiEiendom.py
@@ -0,0 +1,16 @@
+"""
+Created by:     Philip van Schalkwyk
+Contact:        philiplvans@gmail.com
+Last updated:   2021-12-06
+
+The purpose of this script is to crawl property24 (perhaps more websites later) and check the prices
+of property in Still Bay, Western Cape, South Africa.
+
+Some functionality may later include to check the trends of prices.
+
+Maybe play with SQL and calculate average prices over the course of however many days.
+
+I have no idea how I would go about to implement this, so this will consist of reading a lot of
+tutorials and playing around
+"""
+
